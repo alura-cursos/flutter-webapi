9cbdedac643573fd1c100a185db070a954fd5b2a
Alex Felipe <alexfelipevieira@gmail.com>
2019-11-18 08:59:56 -0200

    4.1

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -25,13 +25,15 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+const String baseUrl = 'http://192.168.20.249:8080/transactions';
+
 Future<List<Transaction>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(
-    interceptors: [LoggingInterceptor()],
-  );
-  final Response response = await client
-      .get('http://192.168.20.249:8080/transactions')
-      .timeout(Duration(seconds: 5));
+  final Response response =
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
   for (Map<String, dynamic> transactionJson in decodedJson) {
@@ -47,4 +49,31 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value' : transaction.value,
+    'contact' : {
+      'name' : transaction.contact.name,
+      'accountNumber' : transaction.contact.accountNumber
+    }
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl, headers: {
+    'Content-type': 'application/json',
+    'password': '1000',
+  }, body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bytebank/http/webclient.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
+import 'models/contact.dart';
+
 void main() {
   runApp(BytebankApp());
-  findAll().then((transactions) => print('new transactions $transactions'));
+  save(Transaction(200.0, Contact(0, 'Gui', 2000))).then((transaction) => print(transaction));
 }
 
 class BytebankApp extends StatelessWidget {
