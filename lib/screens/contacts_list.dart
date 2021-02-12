6c49c3c1e0029aa6b7ead677895162a0402a26e7
Alex Felipe <alexfelipevieira@gmail.com>
2021-02-12 10:16:38 -0300

    updating the contact list update

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -4,8 +4,13 @@ import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
-class ContactsList extends StatelessWidget {
+class ContactsList extends StatefulWidget {
 
+  @override
+  _ContactsListState createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
   final ContactDao _dao = ContactDao();
 
   @override
@@ -46,7 +51,7 @@ class ContactsList extends StatelessWidget {
             MaterialPageRoute(
               builder: (context) => ContactForm(),
             ),
-          );
+          ).then((value) => setState(() {}));
         },
         child: Icon(
           Icons.add,
